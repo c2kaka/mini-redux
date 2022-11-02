eba8021ec0c8c67045fc84269bfdf137e269fb2e
OuyangFan <fan.ouyang@transwarp.io>
2022-11-02 20:41:31 +0800

    feat: create reducer function

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,27 @@ const First = () => <section>First<User/></section>
 const Second = () => <section>Second<UserModifier/></section>
 const Third = () => <section>Third</section>
 
+/**
+ * reducer: 用来规范state的创建流程
+ * @param state
+ * @param type
+ * @param payload
+ * @returns {*&{user: (*)}}
+ */
+const reducer = (state, { type, payload }) => {
+  if (type === 'updateUser') {
+    return {
+      ...state,
+      user: {
+        ...state.user,
+        ...payload
+      }
+    };
+  } else {
+    return state;
+  }
+}
+
 const User = () => {
   const contextValue = useContext(appContext);
   return <div>User:{contextValue.appState.user.name}</div>
@@ -28,8 +49,7 @@ const User = () => {
 const UserModifier = () => {
   const { appState, setAppState } = useContext(appContext);
   const onChange = (e) => {
-    appState.user.name = e.target.value;
-    setAppState({...appState})
+    setAppState(reducer(appState, {type: 'updateUser', payload: {name: e.target.value}}))
   }
 
   return <input type="text" value={appState.user.name} onChange={onChange}/>
